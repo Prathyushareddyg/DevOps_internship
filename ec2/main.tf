50ffd2e4d35ed5910881badc41e490e34b44f9a6
Prathyushareddyg <92011622+Prathyushareddyg@users.noreply.github.com>
2022-02-22 21:43:01 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,26 +1,10 @@
-provider "aws" {
-    region = "us-east-1"
+    
+provider "aws"{
+	region = "us-east-1"
+        version = "v2.70.0"
 }
 
-resource "aws_vpc" "vpc_1" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "tf-example1"
-  }
-}
-
-resource "aws_subnet" "subnet_1" {
-  vpc_id            = aws_vpc.vpc_1.id
-  cidr_block        = "10.100.0.0/24"
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "tf-example2"
-  }
-}
-
-resource "aws_instance" "instance_1" {
-  ami           = "ami-033b95fb8079dc481" 
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.subnet_1.id
+resource "aws_instance" "server"{
+	instance_type = "t2.micro"
+	ami = "ami-033b95fb8079dc481"
 }
