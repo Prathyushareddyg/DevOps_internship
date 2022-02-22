133f02208a896a5e000b1e3fc9991558c7cfa8d1
Prathyushareddyg <92011622+Prathyushareddyg@users.noreply.github.com>
2022-02-22 22:02:49 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -2,6 +2,8 @@
 provider "aws"{
 	region = "us-east-1"
         version = "v2.70.0"
+	access_key = "AKIA4YF22HNM4YLAUHER"				
+        secret_key = "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"
 }
 
 resource "aws_instance" "server"{
